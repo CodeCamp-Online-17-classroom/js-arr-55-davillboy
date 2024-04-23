82dfbac823ad87c625383f3c7d4b2d1fc8dce4ad
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 01:37:05 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+let str = 'I live in Thailand';
+let charCount = {};
+
+for (let char of str.toLowerCase()) {
+  if (char !== ' ') {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+}
+
+console.log(charCount);
+
